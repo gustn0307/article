045d451051e7c944901bec19b37c9348636653c4
gustn0307 <gustn0307@hanmail.net>
2026-04-24 10:06:43 +0900

    feat: 메인 클래스의 메인 메서드 구현 및 메인 메서드와 뷰 클래스의 연결 완료

--- src/articleProject/repository/ArticleRepository.java
+++ src/articleProject/repository/ArticleRepository.java
@@ -1,5 +1,16 @@
 package articleProject.repository;
 
+import articleProject.dto.ArticleDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArticleRepository {
+
+    public List<ArticleDto> all(){
+        List<ArticleDto> articleDtos = new ArrayList<>();
+
+        return articleDtos;
+    }
 
 }
